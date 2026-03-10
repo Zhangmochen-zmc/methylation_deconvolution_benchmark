3ebade79cf277575a37dbb0fe109b87fb82e071f
yuxy1321 <yuxy1321@163.com>
2026-03-10 19:27:40 +0800

    Implement MethylCIBERSORT data processing in ref.R
    Added functionality to process methylation data and save results.

--- script/deconvolution/MethylCIBERSORT/ref.R
+++ script/deconvolution/MethylCIBERSORT/ref.R
@@ -1,1 +1,41 @@
+library(tools)           
+library("MethylCIBERSORT")
 
+beta_file <- "test_data.csv" 
+ref_file  <- "ref_raw.RData"
+csv_name <- file_path_sans_ext(basename(beta_file))
+output_dir <- "methylcibersort_ref"
+
+if (!dir.exists(output_dir)) {
+  dir.create(output_dir, recursive = TRUE)
+}
+project <- file.path(output_dir, csv_name)
+
+print(paste("Processing Project:", csv_name))
+print(paste("Output will be saved to prefix:", project))
+
+
+Mat <- read.table(file=beta_file, sep = ',', header = TRUE, row.names=1, check.names=FALSE)
+load(ref_file)  
+RefData <- ref
+RefPheno <- ref.pheno
+
+RefData <- RefData[sort(rownames(RefData)), ]
+Mat <- Mat[sort(rownames(Mat)), ]
+Int <- intersect(rownames(Mat), rownames(RefData))
+Mat <- Mat[match(Int, rownames(Mat)), ]
+RefData <- RefData[match(Int, rownames(RefData)), ]
+
+Signature <- FeatureSelect.V4(
+  CellLines.matrix = NULL,
+  Heatmap = TRUE,
+  export = TRUE,
+  sigName = paste0(csv_name, "_ref"), 
+  Stroma.matrix = RefData,
+  deltaBeta = 0.2,
+  FDR = 0.01,
+  MaxDMRs = 1000,
+  Phenotype.stroma = RefPheno
+)
+
+save(Mat, RefData, RefPheno, Signature, file = paste0(project, "_processed.RData"))
