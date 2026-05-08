433ebe45aa81a454f70a695a025b85b76538268a
yuxy1321 <yuxy1321@163.com>
2026-05-08 14:55:37 +0800

    Update ref.R

--- scripts/deconvolution/Houseman's QC_QP/ref.R
+++ scripts/deconvolution/Houseman's QC_QP/ref.R
@@ -63,7 +63,7 @@ for (cell_type in cell_types) {
   processed_data <- process_methylation_data(base_path)
   
   # Move all generated result files to the specified path.
-  output_dir <- "houseman_ref"
+  output_dir <- "marker_ref"
   file.rename(paste0(base_path, "_merged_methylation_data.txt"), 
               file.path(output_dir, paste0(cell_type, "_merged_methylation_data.txt")))
   cat("The file has been moved to：", file.path(output_dir, paste0(cell_type, "_merged_methylation_data.txt")), "\n")
@@ -122,8 +122,8 @@ process_methylation_matrix <- function(base_path, output_file) {
 }
 
 # Calling functions to process data
-base_path <- "housemman_ref"
-output_file <- "houseman_ref/merged_data.txt"
+base_path <- "marker_ref"
+output_file <- "marker_ref/merged_data.txt"
 result <- process_methylation_matrix(base_path, output_file)
 
 merged_data <- result$merged_data
